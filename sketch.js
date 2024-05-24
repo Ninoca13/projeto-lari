64a320121f95e64f98d9fdea5ceee08bcee02e48
Ninoca13 <00001107112072SP@al.educacao.sp.gov.br>
2024-05-24 08:34:28 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas( 600, 600);
+     background("black");
+}
+function draw() {
+   stroke("blue");
+   fill("red");
+  
+   if(mouseIsPressed){
+   circle(mouseX,mouseY,200,350);
+      }
+   }
+
+    
+     
